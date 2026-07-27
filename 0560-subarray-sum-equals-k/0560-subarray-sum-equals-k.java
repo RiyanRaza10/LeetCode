317ae58e116660c7e6ffe783d0ca41c973625de4
RiyanRaza10 <razariyan28@gmail.com>
2026-07-27 19:53:16 +0530

    Time: 25 ms (46.67%), Space: 48.8 MB (46.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -5,22 +5,23 @@ class Solution {
         //            value : number of subarrays with same prefixsum  
         HashMap<Integer , Integer> map = new HashMap<>();
         
-        int sum = 0 , ans = 0;
+        int prefixSum = 0 , ans = 0;
 
         // Base case -> if sum == k
         map.put(0 , 1);
 
         for(int i=0 ; i<nums.length ; i++){
-            sum += nums[i];
+            prefixSum += nums[i];
 
-            int diff = sum - k;
+            // Required subarray
+            int diff = prefixSum - k;
 
             if(map.containsKey(diff)){
                 ans += map.get(diff);
             }
 
-            // Number of subarrays ending with sum
-            map.put(sum , map.getOrDefault(sum , 0) + 1);
+            // Putting number of subarrays ending with same prefixSum
+            map.put(prefixSum , map.getOrDefault(prefixSum , 0) + 1);
         }
 
         return ans;

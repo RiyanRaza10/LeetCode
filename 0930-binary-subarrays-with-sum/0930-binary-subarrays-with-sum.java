7fd1b547b41f0a2a36dc313ce4a17f11c57faa03
RiyanRaza10 <razariyan28@gmail.com>
2026-08-07 20:10:45 +0530

    Time: 3 ms (55.41%), Space: 50.8 MB (77.54%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,24 +1,66 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        Map<Integer , Integer> map = new HashMap<>();
 
-        int sum = 0 , cnt = 0;
+        // Key Idea :
+        // (Total subarrays with sum == goal) = (Total subarrays with sum <= goal) - (Total subarrays with sum < goal) 
+        int sum = 0 , left = 0 , right = 0 , lessThanEqualToGoalCnt = 0 , equalToGoalCnt = 0;
 
-        // Base Case
-        map.put(0 , 1);
+        // Counting total subarrays with sum <= goal
+        while(right < nums.length){
+            sum += nums[right];
 
-        for(int i=0 ; i<nums.length ; i++){
-            sum += nums[i];
-
-            int req = sum - goal;
-
-            if(map.containsKey(req)){
-                cnt += map.get(req);
+            while(left <= right && sum > goal){
+                sum -= nums[left];
+                left++;
             }
 
-            map.put(sum , map.getOrDefault(sum , 0) + 1);
+            lessThanEqualToGoalCnt += (right - left + 1);
+
+            right++;
         }
 
-        return cnt;
+        left = 0 ; right = 0 ; sum = 0;
+
+        // Counting total subarrays with sum < goal
+        while(right < nums.length){
+            sum += nums[right];
+
+            while(left <= right && sum >= goal){
+                sum -= nums[left];
+                left++;
+            }
+
+            equalToGoalCnt += (right - left + 1);
+
+            right++;
+        }
+
+        // (Total subarrays with sum <= goal) - (Total subarrays with sum < goal)
+        return lessThanEqualToGoalCnt - equalToGoalCnt;
     }
 }
+
+// class Solution {
+//     public int numSubarraysWithSum(int[] nums, int goal) {
+//         Map<Integer , Integer> map = new HashMap<>();
+
+//         int sum = 0 , cnt = 0;
+
+//         // Base Case
+//         map.put(0 , 1);
+
+//         for(int i=0 ; i<nums.length ; i++){
+//             sum += nums[i];
+
+//             int req = sum - goal;
+
+//             if(map.containsKey(req)){
+//                 cnt += map.get(req);
+//             }
+
+//             map.put(sum , map.getOrDefault(sum , 0) + 1);
+//         }
+
+//         return cnt;
+//     }
+// }

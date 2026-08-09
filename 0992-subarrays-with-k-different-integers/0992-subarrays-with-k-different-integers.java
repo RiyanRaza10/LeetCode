bb841d9392418de3c17d6f0f6670f9b0e1f9c971
RiyanRaza10 <razariyan28@gmail.com>
2026-08-09 22:34:57 +0530

    Time: 52 ms (20.47%), Space: 50 MB (74.11%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,19 +1,29 @@
+// Approach - 2 { Updated version of last submission }
+
+// Key Idea : Instead of calculating subarrays with sum < k
+//            we can use the same function for subarrays with sum <= k by calculating subarrays with sum < k-1
+//            Then it will use less lines of code and promotes code reusability
+
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        // Key Idea : 
         // (Total Subarrays with K diff Integers) = (Total Subarrays with less than equal to K diff Integers)
-        //                                        - (Total Subarrays with less than K diff Integers)
+        //                                        - (Total Subarrays with less than K diff Integers 
+        //                                              or less than equal to k-1 Integers)
+        return countSubarray(nums , k) - countSubarray(nums , k-1);        
+    }
 
+    int countSubarray(int[] nums , int k){
 
-        // To store number of count of Integers with their frequency
+        // To keep track of number of different Integers
         HashMap<Integer , Integer> map = new HashMap<>();
 
-        int left = 0 , right = 0 , lessThanEqualToKCnt = 0 , lessThanKCnt = 0;
+        int left = 0 , right = 0 , ans = 0;
 
-        // Counting total Subarrays with less than equal to K diff Integers
+        // Sliding Window
         while(right < nums.length){
             map.put(nums[right] , map.getOrDefault(nums[right] , 0) + 1);
 
+            // Shrinking
             while(left <= right && map.size() > k){
                 map.put(nums[left] , map.get(nums[left]) - 1);
 
@@ -22,35 +32,11 @@ class Solution {
                 left++;
             }
 
-            lessThanEqualToKCnt += (right - left + 1);
+            ans += (right - left + 1);
 
             right++;
         }
 
-        left = 0 ; right = 0 ; 
-
-        // Re-Initialising map so no need of new one
-        map = new HashMap<>();
-
-        // Counting total Subarrays with less than K diff Integers
-        while(right < nums.length){
-            map.put(nums[right] , map.getOrDefault(nums[right] , 0) + 1);
-
-            while(left <= right && map.size() >= k){
-                map.put(nums[left] , map.get(nums[left]) - 1);
-
-                if(map.get(nums[left]) <= 0) map.remove(nums[left]);
-
-                left++;
-            }
-
-            lessThanKCnt += (right - left + 1);
-
-            right++;
-        }
-
-        // (Total Subarrays with less than equal to K diff Integers) - (Total Subarrays with less than K diff Integers)
-        return lessThanEqualToKCnt - lessThanKCnt;
-        
+        return ans;
     }
 }

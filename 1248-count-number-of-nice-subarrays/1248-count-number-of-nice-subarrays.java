1b0fa4c29410444e4eaa02f2125c03d75bfe8b67
RiyanRaza10 <razariyan28@gmail.com>
2026-08-09 19:51:49 +0530

    Time: 14 ms (51.56%), Space: 60 MB (7.48%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,30 +1,73 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        
-        // To store , key : no. of odd numbers till index
-        //            value : no. of subarrays with same count of odd numbers
-        HashMap<Integer , Integer> map = new HashMap<>();
+        int left = 0 , right = 0 , lessThanEqualToKCnt = 0 , lessThanKCnt = 0 , oddCnt = 0;
 
-        int oddCount = 0 , ans = 0;
+        // Counting subarrays with oddCnt <= k
+        while(right < nums.length){
+            if(nums[right] % 2 == 1) oddCnt++;
 
-        // Base case if oddCount == k
-        map.put(0 , 1);
+            while(left <= right && oddCnt > k){
+                if(nums[left] % 2 == 1) oddCnt--;
 
-        for(int i=0 ; i<nums.length ; i++){
-
-            if(nums[i] % 2 == 1) oddCount++;
-
-            // Required subarray
-            int diff = oddCount - k;
-
-            if(map.containsKey(diff)){
-                ans += map.get(diff);
+                left++;
             }
 
-            // Putting no. of subarrays with same no. of oddCount
-            map.put(oddCount , map.getOrDefault(oddCount , 0) + 1);
+            lessThanEqualToKCnt += (right - left + 1);
+
+            right++;            
         }
 
-        return ans;
+        left = 0; right = 0 ; oddCnt = 0;
+
+        // Counting subarrays with oddCnt < k
+        while(right < nums.length){
+            if(nums[right] % 2 == 1) oddCnt++;
+
+            while(left <= right && oddCnt >= k){
+                if(nums[left] % 2 == 1) oddCnt--;
+
+                left++;
+            }
+
+            lessThanKCnt += (right - left + 1);
+
+            right++;            
+        }
+
+        // (Subarrays with oddCnt == k) = (Subarrays with oddCnt <= k) - (Subarrays with oddCnt < k)
+        return lessThanEqualToKCnt - lessThanKCnt;
+
     }
 }
+
+
+// class Solution {
+//     public int numberOfSubarrays(int[] nums, int k) {
+        
+//         // To store , key : no. of odd numbers till index
+//         //            value : no. of subarrays with same count of odd numbers
+//         HashMap<Integer , Integer> map = new HashMap<>();
+
+//         int oddCount = 0 , ans = 0;
+
+//         // Base case if oddCount == k
+//         map.put(0 , 1);
+
+//         for(int i=0 ; i<nums.length ; i++){
+
+//             if(nums[i] % 2 == 1) oddCount++;
+
+//             // Required subarray
+//             int diff = oddCount - k;
+
+//             if(map.containsKey(diff)){
+//                 ans += map.get(diff);
+//             }
+
+//             // Putting no. of subarrays with same no. of oddCount
+//             map.put(oddCount , map.getOrDefault(oddCount , 0) + 1);
+//         }
+
+//         return ans;
+//     }
+// }

4c528318e9a87131efa6f521431a302400ffda9a
RiyanRaza10 <razariyan28@gmail.com>
2026-08-22 19:50:52 +0530

    Time: 12 ms (42.49%), Space: 47.8 MB (31.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,27 +1,54 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
-        // To count freq of character
-        HashMap<Character , Integer> map = new HashMap<>();
+        int[] freq = new int[256];
 
-        int start = 0 , end = 0 , maxLen = 0;
+        int left = 0 , right = 0 , maxLen = 0;
 
-        while(end < s.length()){
-            char ch = s.charAt(end);
+        while(right < s.length()){
+            char ch = s.charAt(right);
+            
+            freq[ch]++;
 
-            if(map.containsKey(ch)){
-                start = Math.max(start , map.get(ch) + 1);
+            while(left <= right && freq[ch] > 1){
+                
+                freq[s.charAt(left)]--;
+
+                left++;
             }
 
-            maxLen = (end - start + 1) > maxLen ? end - start + 1 : maxLen;
+            maxLen = Math.max(maxLen , right - left + 1);
 
-            // Putting element with its index
-            map.put(ch , end);
-
-            end++;
+            right++;
         }
 
         return maxLen;
-
     }
 }
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+        
+//         // To count freq of character
+//         HashMap<Character , Integer> map = new HashMap<>();
+
+//         int start = 0 , end = 0 , maxLen = 0;
+
+//         while(end < s.length()){
+//             char ch = s.charAt(end);
+
+//             if(map.containsKey(ch)){
+//                 start = Math.max(start , map.get(ch) + 1);
+//             }
+
+//             maxLen = (end - start + 1) > maxLen ? end - start + 1 : maxLen;
+
+//             // Putting element with its index
+//             map.put(ch , end);
+
+//             end++;
+//         }
+
+//         return maxLen;
+
+//     }
+// }

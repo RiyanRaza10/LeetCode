f4e768043abe080a19d3ddb887af69b4b5a81001
RiyanRaza10 <razariyan28@gmail.com>
2026-08-29 17:10:16 +0530

    Time: 28 ms (12.57%), Space: 47.2 MB (9.6%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,75 @@
+class Solution {
+
+    int factorial(int n){
+        int prod = 1;
+
+        while(n > 0){
+            prod *= n;
+            n--;
+        }
+
+        return prod;
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Set<List<Integer>> uniquePermutations = new HashSet<>();
+
+        int totalPermutations = factorial(nums.length);
+
+        while(totalPermutations-- > 0){
+            List<Integer> currPermutation = new ArrayList<>();
+
+            for(int val : nums) currPermutation.add(val);
+
+            uniquePermutations.add(currPermutation);
+
+            nextPermutation(nums);
+        }
+
+        return new ArrayList<>(uniquePermutations);
+        
+    }
+
+    void nextPermutation(int[] nums){
+        int ind = -1 , n = nums.length;
+
+        for(int i=n-2 ; i>=0 ; i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+
+        if(ind == -1){
+            reverse(nums , 0 , n-1);
+
+            return;
+        }
+
+        for(int i=n-1 ; i>ind ; i--){
+            if(nums[i] > nums[ind]){
+                int temp = nums[i];
+                nums[i] = nums[ind];
+                nums[ind] = temp;
+
+                break;
+            }
+        }
+
+        reverse(nums , ind+1 , n-1);
+        
+    }
+
+    void reverse(int[] nums , int start , int end){
+
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+
+    }
+}

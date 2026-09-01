f163f23018018d9d8f339f9c7ef0fbba9a740b91
RiyanRaza10 <razariyan28@gmail.com>
2026-09-01 16:27:04 +0530

    Time: 0 ms (100%), Space: 50.2 MB (47.38%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,23 +1,8 @@
+// Optimised log(n) solution
+
 class Solution {
-
-    boolean isPossible(int[] citations , int minCitations){
-        int totalPapers = 0;
-
-        for(int i=citations.length -1 ; i>=0 ; i--){
-            if(citations[i] >= minCitations){
-                totalPapers++;
-            }
-
-            if(totalPapers == minCitations) break;
-
-        }
-
-        return totalPapers == minCitations;
-        
-    }
-
     public int hIndex(int[] citations) {
-        int left = 1 , right = citations.length;
+        int left = 0 , right = citations.length-1 , n = citations.length;
 
         int ans = 0;
 
@@ -25,12 +10,13 @@ class Solution {
             int mid = left + (right - left) / 2;
 
             // Valid Citation found, search larger
-            if(isPossible(citations , mid)){
-                ans = mid;
-                left = mid + 1;
+            // n - mid = total research papers from last till index mid
+            if(citations[mid] >= n - mid){
+                ans = n - mid;
+                right = mid - 1;
             }
 
-            else right = mid - 1;
+            else left = mid + 1;
         }
 
         return ans;

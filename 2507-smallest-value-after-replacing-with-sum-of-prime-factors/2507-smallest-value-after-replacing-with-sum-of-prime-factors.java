25b51164cd47e9be934ae3abd7751bb7b73976a9
RiyanRaza10 <razariyan28@gmail.com>
2026-09-01 23:39:04 +0530

    Time: 2 ms (46.03%), Space: 42.4 MB (37.7%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -1,51 +1,28 @@
 class Solution {
 
-    int sumPrimes(int n){
+    public int smallestValue(int n) {
         int sum = 0 , temp = n;
 
-        boolean []isPrime = new boolean[n+1];
-        Arrays.fill(isPrime , true);
-
-        isPrime[0] = isPrime[1] = false;
-
-        for(int i=2 ; i*i <= n ; i++){
-            if(isPrime[i]){
-
-                int j = 2*i;
-
-                while(j <= n){
-                    isPrime[j] = false;
-                    j += i;
-                }
-            }
-        }
-
         for(int i=2 ; i<=n ; i++){
-            if(isPrime[i] && n % i == 0){
-                
-                while(n % i == 0){
-                    System.out.println("i : " + i) ;
-                    sum += i;
-                    n /= i;
-                }
-            }
+            
+            while(n % i == 0){
+                sum += i;
+                n /= i;
+            }                
+            
         }
 
-        return sum == temp ? -1 : sum;
-    }
+        return temp == sum ? sum : smallestValue(sum);
 
-    public int smallestValue(int n) {
-
-        while(n != 0){
-
-            int curr = sumPrimes(n);
-
-            if(curr == -1) return n;
-
-            n = curr;
-
-        }
-
-        return n;
     }
 }
+
+ // while(n != 0){
+
+        //     int curr = sumPrimes(n);
+
+        //     if(curr == -1) return n;
+
+        //     n = curr;
+
+        // }

7d34cefdbee9990466341801e9eff8ca0931c12f
RiyanRaza10 <razariyan28@gmail.com>
2026-09-11 18:44:18 +0530

    Time: 14 ms (9.98%), Space: 73.8 MB (20.06%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -1,42 +1,24 @@
 class Solution {
-
-    char SHIFT(char ch , long n){
-        
-        long shift = (int)(ch) + n ;
-        
-        // if(shift > 122){
-        //     shift -= 26;
-        // }
-        // return (char)(shift);
-
-        return shift <= 122 ? (char)(shift) : (char)(shift-26) ;
-    }
-
     public String shiftingLetters(String s, int[] shifts) {
-        
-        long total_shifts = 0;
+        int n = shifts.length;
+        long totalShifts = 0;
 
         StringBuilder res = new StringBuilder();
 
-        // Precomputing total shifts
-        for(int val : shifts){
-            // As val can be too large 
-            total_shifts += (val % 26);
-        }
+        for(int i=n-1 ; i>=0 ; i--){
+            totalShifts += shifts[i];
 
-        // Shifting 
-        for(int i=0 ; i<s.length() ; i++){
-            char ch = s.charAt(i);   
+            // To avoid overflow
+            totalShifts %= 26; 
+
+            // Shifted char
+            int shiftedChar = (int)totalShifts + s.charAt(i);  
             
-            // Shifting character (mod is used to prevent overflow of letters)
-            res.append( SHIFT(ch , total_shifts%26 ) );
-            
-            // Reducing shifts for each character (mod is used to prevent overflow of letters)
-            total_shifts -= (shifts[i] % 26);
+            res.append(shiftedChar <= 122 ? (char)shiftedChar : (char)(shiftedChar - 26));
             
         }
 
-        return res.toString();
+        return res.reverse().toString();
 
     }
 }

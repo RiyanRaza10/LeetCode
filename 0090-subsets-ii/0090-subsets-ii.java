2925a6350d2b208fe283ca20f7d65ae9248eb6ce
RiyanRaza10 <razariyan28@gmail.com>
2026-09-12 21:40:05 +0530

    Time: 2 ms (99.74%), Space: 45.1 MB (59.61%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,14 +2,14 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
 
-        Set<List<Integer>> uniqueSubsets = new HashSet<>();
+        List<List<Integer>> uniqueSubsets = new ArrayList<>();
 
         backtrack(uniqueSubsets , new ArrayList<>() , nums , 0);
 
         return new ArrayList<>(uniqueSubsets);
     }
 
-    void backtrack(Set<List<Integer>> uniqueSubsets , List<Integer> currSubset , int[] nums , int ind){
+    void backtrack(List<List<Integer>> uniqueSubsets , List<Integer> currSubset , int[] nums , int ind){
 
         // Base Case
         if(ind == nums.length){
@@ -21,6 +21,9 @@ class Solution {
 
         // Pick
         backtrack(uniqueSubsets , currSubset , nums , ind+1);
+
+        // Avoid duplicates
+        while(ind < nums.length-1 && nums[ind] == nums[ind+1]) ind++;
 
         // Remove used element
         currSubset.remove(currSubset.size()-1);

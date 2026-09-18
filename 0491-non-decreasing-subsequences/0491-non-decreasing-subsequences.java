972f3d45781a12e132db3b1776ef9eb3a7b14675
RiyanRaza10 <razariyan28@gmail.com>
2026-09-18 23:03:37 +0530

    Time: 13 ms (43.67%), Space: 53.2 MB (39.9%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> allCombinations = new HashSet<>();
+
+        backtrack(allCombinations , new ArrayList<>() , nums , 0);
+
+        return new ArrayList<>(allCombinations);
+    }
+
+    void backtrack(Set<List<Integer>> allCombinations , List<Integer> currCombination , int[] nums , int ind){
+
+        // Check if subsequence is non-decreasing
+        if(currCombination.size() >= 2 && currCombination.get(currCombination.size()-1) < currCombination.get(currCombination.size()-2)) return;
+
+        // Poosible combination
+        if(ind == nums.length){
+            if(currCombination.size() >= 2) allCombinations.add(new ArrayList<>(currCombination));
+
+            return;
+        }
+
+        currCombination.add(nums[ind]);
+
+        // Pick
+        backtrack(allCombinations , currCombination , nums , ind+1);
+
+        currCombination.removeLast();
+
+        // Skip duplicates
+        while(ind < nums.length-1 && nums[ind] == nums[ind+1]) ind++;
+
+        // Not pick
+        backtrack(allCombinations , currCombination , nums , ind+1);
+    }
+}

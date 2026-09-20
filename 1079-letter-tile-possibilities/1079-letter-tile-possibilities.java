1db42c6f61c2959ff1a4745b363d9ddd401e326d
RiyanRaza10 <razariyan28@gmail.com>
2026-09-20 09:57:29 +0530

    Time: 23 ms (20.38%), Space: 46.9 MB (26.11%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -1,58 +1,32 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
-        Set<String> set = new HashSet();
+        Set<String> allTiles = new HashSet<>();
 
-        backtrack(set , new StringBuilder() , tiles , tiles.length() , 0);
+        boolean[] used = new boolean[tiles.length()];
 
-        return set.size();
+        solve(allTiles , new StringBuilder() , tiles , used);
+
+        return allTiles.size();
     }
 
-    void backtrack(Set<String> set , StringBuilder res , String tiles , int n , int ind){
+    void solve(Set<String> allTiles , StringBuilder res , String tiles , boolean[] used){
 
-        if(ind == n){
-
-            if(res.length() == 0) return;
-
-            // Add all permutations of res to set
-            if(!set.contains(res.toString())){
-                boolean[] used = new boolean[res.length()];
-                permute(set , res.toString() , new StringBuilder() , used , res.length() , 0);
-            }
-            
-            set.add(res.toString());
-
-            return;
-        }
-
-        for(int i=ind ; i<n ; i++){
-            res.append(tiles.charAt(i));
-
-            backtrack(set , res , tiles , n , i+1);
-
-            res.deleteCharAt(res.length()-1);
-            
-            backtrack(set , res , tiles , n , i+1);
-        } 
-    }
-
-    void permute(Set<String> set , String s , StringBuilder curr , boolean[] used , int n , int ind){
-
-        if(curr.length() == s.length()){
-            set.add(curr.toString());
-
-            return;
-        }
-
-        for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<tiles.length() ; i++){
             if(!used[i]){
+                
+                // Use current tile / pick
                 used[i] = true;
-                curr.append(s.charAt(i));
+                res.append(tiles.charAt(i));
 
-                permute(set , s , curr , used , n , i+1);
+                // Add every possible tile to set
+                allTiles.add(res.toString());
 
-                // backtrack
+                // Recursive call
+                solve(allTiles , res , tiles , used);
+
+                // Backtrack / not pick
                 used[i] = false;
-                curr.deleteCharAt(curr.length()-1);
+                res.deleteCharAt(res.length()-1);
             }
         }
     }

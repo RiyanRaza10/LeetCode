9a6da602a6454dd0f4851a4395da7104937b0675
RiyanRaza10 <razariyan28@gmail.com>
2026-09-24 23:38:09 +0530

    Time: 50 ms (10.48%), Space: 79.2 MB (45.44%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,55 +1,38 @@
 class Solution {
-
-    boolean canPlaceBalls(int[] position , int balls , int minGap){
-        // One ball is always placed at zero
+    boolean canPlaceAll(int[] position , int balls , int maxDist){
         balls--;
 
-        // We will place balls only if the gap between two balls is atleast "minGap"
-        int prevPosition = position[0];
+        int prev = position[0];
 
         for(int i=1 ; i<position.length ; i++){
-
-            // We will place balls only if the gap in position of two balls is atleast "minGap"
-            if(position[i] - prevPosition >= minGap){
-                prevPosition = position[i];
-                
+            if(position[i] - prev >= maxDist){
                 balls--;
+                prev = position[i];
             }
 
-            // All balls placed , no need to further check
-            if(balls == 0) break;
+            if(balls <= 0) break;
         }
 
-        // Check if all balls are placed
-        return balls == 0;
-    
+        return balls <= 0;
     }
-
+    
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
 
-        int n = position.length;
-
-        int left = 1 , right = position[n-1] - position[0];
-
-        int maxForce = -1;
+        int left = 0 , right = position[position.length-1] , ans = 0;
 
         while(left <= right){
             int mid = left + (right - left) / 2;
 
-            // m balls can be placed with each ball having a minimum gap of mid
-            // So increase gap to get maximum result
-            if(canPlaceBalls(position , m , mid)){
-                maxForce = mid;
-
+            if(canPlaceAll(position , m , mid)){
                 left = mid + 1;
+                ans = mid;
             }
 
             else right = mid - 1;
 
         }
 
-        return maxForce;
-        
+        return ans;
     }
 }
